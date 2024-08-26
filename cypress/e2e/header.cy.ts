b8c0ecd6891e84181d469dfc758204b2c39c0d1c
l0x539 <nogues322@gmail.com>
2024-08-26 14:36:40 +0400

    [Add] Api route.

---
+++ cypress/e2e/header.cy.ts
@@ -0,0 +1,15 @@
+describe('template spec', () => {
+  it('passes', () => {
+    // // Start from the index page
+    // cy.visit('http://localhost:3000/')
+
+    // // Find a link with an href attribute containing "about" and click it
+    // cy.get('a[href*="#about"]').click()
+  
+    // // The new url should include "/about"
+    // cy.url().should('include', '#about')
+  
+    // // The new page should contain an h1 with "About"
+    // cy.get('div.uppercase.w-64.text-xl').contains('about project')
+  })
+})
